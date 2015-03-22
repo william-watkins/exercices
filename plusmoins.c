05559843a0b28c10dbbd6867fcbb5036c554fd80
william-watkins <william@watkins.fr>
2015-03-22 19:22:44 +0100

    file plusmoin added

---
+++ plusmoins.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+
+int difficulty();
+void game_logic(int max);
+bool core(int number, int counter);
+
+int main()
+{
+    int max;
+    char mainloop;
+
+    mainloop = 'y';
+
+    while (mainloop == 'y')
+    {
+		max = difficulty();
+
+		if (max == 666)
+		{
+			printf("> incorrect input\n");
+		    return EXIT_FAILURE;
+		}
+		game_logic(max);
+	
+		printf("> Play again ? [y/n]");
+		scanf("%c", &mainloop);
+		scanf("%c", &mainloop);
+    }
+    return EXIT_SUCCESS;
+}
+
+
+/* set the difficulty menu for the game */
+int difficulty()
+{
+	int choice;
+
+	printf("========<  NOMBRE MYSTERE  >========\n");
+ 	printf("= 1: EASY                          =\n");
+ 	printf("= 2: MEDIUM                        =\n");
+ 	printf("= 3: HARD                          =\n");
+ 	printf("====================================\n");
+  	printf("> ");
+  	scanf("%d", &choice);
+  	if (choice == 1)
+	{
+	    return 100;
+  	}
+  	else if (choice == 2)
+	{
+	    return 1000;
+  	}
+	else if (choice == 3)
+	{
+	    return 10000;
+  	}
+ 	else 
+	{
+	    return 666;
+	}
+}
+
+
+
+void game_logic(int max)
+{
+	int number;
+	int min;
+	int counter;
+	bool condition;
+
+	min = 1;
+	counter = 1;
+	condition = false;
+
+	srand(time(NULL));
+	number = (rand() % (max - min + 1)) + min;
+
+	printf("-- the game begin --");
+
+	while (condition == false)
+	{
+		condition = core(number, counter);
+	    counter++;
+	}
+}
+
+
+
+bool core(int number, int counter)
+{
+	int input;
+
+	printf("\n> proposition: ");
+	scanf("%d", &input);
+	    
+	if (input < number)
+	{
+		printf("mystery number is more.");
+	}
+	else if (input > number)
+	{
+		printf("mystery number is less.");
+	}
+	else if (input == number)
+	{
+		printf("correct, mystery number is %d, found in %d try.\n", number, counter);
+		return true;
+	}
+}
