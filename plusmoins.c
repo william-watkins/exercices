8648cb156e4cd86057efb147be59acd9c5fe45a0
william-watkins <william@watkins.fr>
2015-03-22 21:55:27 +0100

    minor changes

--- plusmoins.c
+++ plusmoins.c
@@ -3,69 +3,68 @@
 #include <time.h>
 #include <stdbool.h>
 
-int difficulty();
-void game_logic(int max);
-bool core(int number, int counter);
+int ft_difficulty();
+void ft_game_logic(int max);
+bool ft_core(int number, int counter);
 
 int main()
 {
-    int max;
-    char mainloop;
+	int max;
+	char main_loop;
 
-    mainloop = 'y';
+	main_loop = 'y';
 
-    while (mainloop == 'y')
-    {
-		max = difficulty();
+	while (main_loop == 'y')
+	{
+		max = ft_difficulty();
 
 		if (max == 666)
 		{
 			printf("> incorrect input\n");
-		    return EXIT_FAILURE;
+			return EXIT_FAILURE;
 		}
-		game_logic(max);
+		ft_game_logic(max);
 	
 		printf("> Play again ? [y/n]");
-		scanf("%c", &mainloop);
-		scanf("%c", &mainloop);
-    }
-    return EXIT_SUCCESS;
+		scanf("%c", &main_loop);
+		scanf("%c", &main_loop);
+	}
+	return EXIT_SUCCESS;
 }
 
 
 /* set the difficulty menu for the game */
-int difficulty()
+int ft_difficulty()
 {
 	int choice;
 
 	printf("========<  NOMBRE MYSTERE  >========\n");
- 	printf("= 1: EASY                   [1;100]=\n");
- 	printf("= 2: MEDIUM                [1;1000]=\n");
- 	printf("= 3: HARD                 [1;10000]=\n");
- 	printf("====================================\n");
-  	printf("> ");
-  	scanf("%d", &choice);
+	printf("= 1: EASY                   [1;100]=\n");
+	printf("= 2: MEDIUM                [1;1000]=\n");
+	printf("= 3: HARD                 [1;10000]=\n");
+	printf("====================================\n");
+	printf("> ");
+	scanf("%d", &choice);
   	if (choice == 1)
 	{
-	    return 100;
+		return 100;
   	}
   	else if (choice == 2)
 	{
-	    return 1000;
+		return 1000;
   	}
 	else if (choice == 3)
 	{
-	    return 10000;
+		return 10000;
   	}
  	else 
 	{
-	    return 666;
+		return 666;
 	}
 }
 
 
-
-void game_logic(int max)
+void ft_game_logic(int max)
 {
 	int number;
 	int min;
@@ -83,14 +82,13 @@ void game_logic(int max)
 
 	while (condition == false)
 	{
-		condition = core(number, counter);
-	    counter++;
+		condition = ft_core(number, counter);
+		counter++;
 	}
 }
 
 
-
-bool core(int number, int counter)
+bool ft_core(int number, int counter)
 {
 	int input;
 
@@ -111,3 +109,4 @@ bool core(int number, int counter)
 		return true;
 	}
 }
+
